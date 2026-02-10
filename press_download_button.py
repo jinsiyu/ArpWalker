b74a2a106cb5dbbac39518b87398c7a78e54dcc5
jinsiyu <jinsiyu940203@163.com>
2026-02-10 13:39:29 +0800

    修改附件下载逻辑，添加对应分类命名

--- press_download_button.py
+++ press_download_button.py
@@ -11,18 +11,30 @@ def press_download_button(scraper):
         scraper.driver.execute_script("arguments[0].scrollIntoView({block: 'center'});", download_button)
         # 执行JavaScript代码获取每个下载按钮对应的Angular对象数据
         # 使用execute_script执行JavaScript代码
-        angular_data = scraper.driver.execute_script("""
+        obj_data = scraper.driver.execute_script("""
                 var element = arguments[0];
                 var scope = angular.element(element).scope();
                 return scope.obj ? JSON.stringify(scope.obj, null, 2) : 'No obj found';
             """, download_button)
 
         print(f"第{i + 1}个下载按钮的Angular对象数据:")
-        print(angular_data)
+        print(obj_data)
+        print("-" * 50)
+
+        # 获取并输出item数据
+        item_data = scraper.driver.execute_script("""
+                var element = arguments[0];
+                var scope = angular.element(element).scope();
+                return scope.item ? JSON.stringify(scope.item, null, 2) : 'No item found';
+            """, download_button)
+
+        print(f"第{i + 1}个下载按钮的item数据:")
+        print(item_data)
         print("-" * 50)
         # 解析Angular对象数据
         try:
-            obj_data = eval(angular_data) if angular_data != 'No obj found' else {}
+            obj_data = eval(obj_data) if obj_data != 'No obj found' else {}
+            item_data_dict = eval(item_data) if item_data != 'No item found' else {}
             if 'APPENDIX_URL' in obj_data and 'APPENDIX_NAME' in obj_data:
                 appendix_name = obj_data['APPENDIX_NAME']
                 # 如果appendix_name以PJ-开头，则跳过下载
@@ -49,6 +61,9 @@ def press_download_button(scraper):
                     elif 'APPENDIX_TYPE' in obj_data and obj_data['APPENDIX_TYPE'] == 'payImg':
                         # 对于APPENDIX_TYPE为payImg的文件，构造新的文件名
                         new_appendix_name = f"购物截图附件和情况说明_{obj_data['APPENDIX_NAME']}"
+                    elif 'RECEIPT_CODE' in item_data_dict and 'SERIAL_NUMBER' in item_data_dict and 'AMT' in item_data_dict:
+                        # 检查item数据中是否存在RECEIPT_CODE、SERIAL_NUMBER和AMT字段，构造新的文件名
+                        new_appendix_name = f"银行回单_{item_data_dict['RECEIPT_CODE']}_{item_data_dict['SERIAL_NUMBER']}_{item_data_dict['AMT']}"
                     else:
                         # 不符合要求的附件文件名改为附件_APPENDIX_NAME
                         new_appendix_name = f"附件_{obj_data['APPENDIX_NAME']}"
